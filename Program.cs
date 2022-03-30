bdb429a371e4ba26c17b1b3504225f3287ea361a
shrey2594 <shrey2510@gmail.com>
2022-03-31 05:29:56 +0530

    Employee wages for multiple companies

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageMultipleCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program.");
+        }
+    }
+
+}
