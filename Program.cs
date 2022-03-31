98f0fd2bf30764f41edb0143a4cc809eecc1cd7b
shrey2594 <shrey2510@gmail.com>
2022-03-31 05:55:07 +0530

    Employee Wage UC2 Assignment

--- Program.cs
+++ Program.cs
@@ -7,21 +7,39 @@ namespace EmployeeWageMultipleCompany
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program.");
-            checkPresentAbsent();
+            CalculateDailyWage();
         }
 
-        public static void checkPresentAbsent()
+        public static bool checkPresentAbsent()
         {
             int isPresent = 1;
             Random random = new Random();
             int check = random.Next(0, 2);
             if (check == isPresent)
             {
-                Console.WriteLine("Employee is presnt.");
+                Console.WriteLine("Employee is present.");
+                return true;
             }
             else
             {
                 Console.WriteLine("Employee is absent.");
+                return false;
+            }
+        }
+
+        public static void CalculateDailyWage()
+        {
+            int wagePerHour = 20;
+            int fullDayHour = 8;
+            int moneyEarned;
+            if (checkPresentAbsent() == true)
+            {
+                moneyEarned = wagePerHour * fullDayHour;
+                Console.WriteLine("The employee earned " + moneyEarned + " throughout the day.");
+            }
+            else
+            {
+                Console.WriteLine("The employee is Absent, hence earned nothing.");
             }
         }
     }
