f0260393f48cc84a1288fa2b7f15c64d1301969d
shrey2594 <shrey2510@gmail.com>
2022-03-31 05:45:53 +0530

    Employee Wage UC1 Assignment

--- Program.cs
+++ Program.cs
@@ -7,6 +7,22 @@ namespace EmployeeWageMultipleCompany
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program.");
+            checkPresentAbsent();
+        }
+
+        public static void checkPresentAbsent()
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == isPresent)
+            {
+                Console.WriteLine("Employee is presnt.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent.");
+            }
         }
     }
 
